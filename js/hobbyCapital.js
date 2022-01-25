2c69c2de7cbe0ac1184555355de8f640958e9dc2
Jillbills <87965054+Jillbills@users.noreply.github.com>
2022-01-25 14:25:57 -0800

    Coded homepage and most of vinyl decals get started pages

---
+++ js/hobbyCapital.js
@@ -0,0 +1,38 @@
+// STICKY HEADER feature - shows hero img first, then scroll to stick header.
+window.onscroll = function() {myFunction()};
+    
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+};
+
+
+
+
+
+// BACK TO TOP BUTTON 
+//Get the button:
+mybutton = document.getElementById("bttBtn");
+
+// When the user scrolls down 100px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
